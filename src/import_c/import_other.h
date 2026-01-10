4910be26fd77b9b60b9b3f94a7808cfe2e42e953
xLeaves <xywhsoft@qq.com>
2026-01-10 16:22:11 +0800

    添加热加载功能，提供部分说明文档

--- src/import_c/import_other.h
+++ src/import_c/import_other.h
@@ -1,3 +1,9 @@
+// 热加载函数前向声明
+int xsReloadHost(XS_ServerObject objServer, XS_HostObject objHost);
+int xsReloadHostByDomain(XS_ServerObject objServer, const char* sDomain);
+int xsReloadDefaultHost(XS_ServerObject objServer);
+int xsReloadServer(XS_ServerObject objServer);
+
 void ImportOther(TCCState* s)
 {
 	// TCC 状态机管理
@@ -8,4 +14,9 @@ void ImportOther(TCCState* s)
 	tcc_add_symbol(s, "http_reply", http_reply);
 	tcc_add_symbol(s, "ParseCookies", ParseCookies);
 	tcc_add_symbol(s, "FreeCookies", FreeCookies);
+	// 热加载函数
+	tcc_add_symbol(s, "xsReloadHost", xsReloadHost);
+	tcc_add_symbol(s, "xsReloadHostByDomain", xsReloadHostByDomain);
+	tcc_add_symbol(s, "xsReloadDefaultHost", xsReloadDefaultHost);
+	tcc_add_symbol(s, "xsReloadServer", xsReloadServer);
 }
